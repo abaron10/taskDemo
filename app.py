3318bd1a5e0729f826280d15bcf92373be8925bd
Andres Barón <af.baron10@uniandes.edu.co>
2021-02-07 17:48:26 -0500

    Aplicacion funcional.

--- app.py
+++ app.py
@@ -23,6 +23,17 @@ def create():
     db.session.commit()
     db.session.close()
     return redirect(url_for('home'))
+@app.route('/delete/<id>')
+def delete(id):
+   task = Task.query.filter_by(id = int(id)).delete()
+   db.session.commit()
+   return redirect(url_for('home'))
+@app.route('/done/<id>')
+def done(id):
+   task = Task.query.filter_by(id = int(id)).first()
+   task.done = not(task.done)
+   db.session.commit()
+   return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
