e664d422a80b0a625e7ce4ca22068e2bf1cb6683
Andres Barón <af.baron10@uniandes.edu.co>
2021-02-07 17:30:02 -0500

    Base de datos funcional.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,json,request,url_for
+from flask import Flask, render_template,json,request,url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -7,10 +7,22 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///database/task.db'
 db = SQLAlchemy(app)
 
+class Task(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200))
+    done = db.Column(db.Boolean)
 @app.route('/')
 def home():
-    return render_template('index.html', title='About')
+    tasks = Task.query.all()
+    return render_template('index.html', tasks=tasks)
 
+@app.route('/create-task',methods=['POST'])
+def create():
+    task = Task(content=request.form['content'], done=False)
+    db.session.add(task)
+    db.session.commit()
+    db.session.close()
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
     app.run(debug=True)
